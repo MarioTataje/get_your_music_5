12eaf67b74331ce1f5be1b3d2f771a02da9c63e1
MGTO <magotor1304@gmail.com>
2020-11-05 03:01:15 -0500

    this

--- src/main/kotlin/com/get_your_music_5/social_system/models/Following.kt
+++ src/main/kotlin/com/get_your_music_5/social_system/models/Following.kt
@@ -1,5 +1,23 @@
 package com.get_your_music_5.social_system.models
 
+import com.get_your_music_5.users_system.models.Musician
+import javax.persistence.*
 
-class Following {
-}
+@Entity
+@Table(name = "followings")
+class Following(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long=0,
+
+        @ManyToOne
+        @JoinColumn(name = "follower_id")
+        var follower: Musician? = null,
+
+        @ManyToOne
+        @JoinColumn(name = "followed_id")
+        var followed: Musician? = null,
+
+        @Column(name = "follow_date", nullable = false)
+        var followDate: String = ""
+)
--- src/main/kotlin/com/get_your_music_5/users_system/models/Musician.kt
+++ src/main/kotlin/com/get_your_music_5/users_system/models/Musician.kt
@@ -3,6 +3,7 @@ package com.get_your_music_5.users_system.models
 import com.get_your_music_5.contracts_system.models.Contract
 import com.get_your_music_5.media_system.models.Genre
 import com.get_your_music_5.media_system.models.Instrument
+import com.get_your_music_5.social_system.models.Following
 import java.io.Serializable
 import javax.persistence.*
 
